2bf8a97bafc21a8595b9fff079793317694ed216
Kai Roth <kai@kana.jetzt>
2023-02-20 23:58:03 +0100

    🔨 update paths if new mod name

--- godot-mod-export-tool-plugin/godot-3/addons/godot-mod-export/store.gd
+++ godot-mod-export-tool-plugin/godot-3/addons/godot-mod-export/store.gd
@@ -4,24 +4,34 @@ class_name ModToolStore
 
 const PATH_SAVE_FILE = "user://mod-tool-plugin-save.json"
 
-var name_mod_dir := ""
+var name_mod_dir := "" setget set_name_mod_dir
 var path_mod_dir := ""
 var path_export_dir := ""
 var path_project_dir := ""
 var path_temp_dir := ""
 var excluded_file_extensions : PoolStringArray = [".csv.import"]
-var path_mod_files : PoolStringArray = []
+var path_mod_files : Array = []
 
 var label_output : RichTextLabel
 
+
+func set_name_mod_dir(new_name_mod_dir):
+	update_paths(new_name_mod_dir)
+	
 
 func init(store: Dictionary):
 	name_mod_dir = store.name_mod_dir
 	path_mod_dir = "res://mods-unpacked/" + store.name_mod_dir
 	path_export_dir = "res://zips"
-	path_project_dir = ModToolUtils.get_local_folder_dir()
+	path_project_dir = ProjectSettings.globalize_path(ModToolUtils.get_local_folder_dir())
 	path_temp_dir = "user://temp/" + store.name_mod_dir
 	excluded_file_extensions = [".csv.import"]
+
+
+func update_paths(new_name_mod_dir):
+	name_mod_dir = new_name_mod_dir
+	path_mod_dir = "res://mods-unpacked/" + new_name_mod_dir
+	path_temp_dir = "user://temp/" + new_name_mod_dir
 
 
 func save_store():
