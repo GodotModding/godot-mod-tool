6a37c08d85b6fb70cb23048dbcf679424181237b
Kai Roth <kai@kana.jetzt>
2023-02-23 09:49:03 +0100

    ✔ using `ConfigFile` Class to get imported file

--- addons/mod_tool/build_zip.gd
+++ addons/mod_tool/build_zip.gd
@@ -22,13 +22,13 @@ func build_zip(store: ModToolStore) -> void:
 			var path_imported_file := _get_imported_file_path(store, path_mod_file)
 			# And add it to the mod file paths
 			if not path_imported_file == "":
-				store.path_mod_files.append("res://" + path_imported_file)
+				store.path_mod_files.append(path_imported_file)
 
 	# Generate temp folder that get's zipped later
 	for i in store.path_mod_files.size():
 		var path_mod_file := store.path_mod_files[i] as String
 
-		# copy mod_file to path_zip_file
+		# Copy mod_file to path_zip_file
 		ModToolUtils.file_copy(path_mod_file, store.path_temp_dir + '/' + path_mod_file.trim_prefix("res://"))
 
 	# Zip that folder with 7zip
@@ -46,36 +46,18 @@ func build_zip(store: ModToolStore) -> void:
 	ModToolUtils.remove_recursive(path_global_temp_dir)
 
 
-# TODO: Rework this to use [ConfigFile]
-# https://docs.godotengine.org/en/stable/classes/class_configfile.html
 func _get_imported_file_path(store: ModToolStore, import_file_path: String) -> String:
+	var config := ConfigFile.new()
+
 	# Open file
-	var text := ModToolUtils.file_get_as_text(import_file_path)
+	var error := config.load(import_file_path)
+
+	if error != OK:
+		ModToolUtils.output_error(store, "Failed to load import file -> " + str(error))
 
 	# Get the path to the imported file
-	var path_imported_file_regex_results := ModToolUtils.get_regex_results(text, "res\\:\\/\\/\\.import.+?(?=\\\")")
-	if(path_imported_file_regex_results.size() == 0):
-		print("ERROR: No path found inside .import file! import_file_path -> " + import_file_path)
-		return ""
-
-	# Grab the first path to the imported file
 	# Imported file example path:
 	# res://.import/ImportedPNG.png-eddc81c8e2d2fc90950be5862656c2b5.stex
-	var imported_file_src_path_local := path_imported_file_regex_results[0] as String
-	# remove the res://
-	imported_file_src_path_local = imported_file_src_path_local.replace('res://', '')
-	# Globalize the path:
-	# C:\Path\to\GodotProject\.import\ImportedPNG.png-eddc81c8e2d2fc90950be5862656c2b5.stex
-	var imported_file_src_path := str(store.path_project_dir, imported_file_src_path_local)
+	var imported_file_path := config.get_value('remap', 'path') as String
 
-	# Get the imported file dst file - the dst path is local to the zip file
-	# Imported file dst path example:
-	# \.import\ImportedPNG.png-eddc81c8e2d2fc90950be5862656c2b5.stex
-
-	# ['C:\Path\to\GodotProject\', '\ImportedPNG.png-eddc81c8e2d2fc90950be5862656c2b5.stex']
-	var imported_file_src_path_split := imported_file_src_path.split('.import')
-	var imported_file_local_path := imported_file_src_path_split[1]
-
-	var imported_file_dst_path := str('.import', imported_file_local_path)
-
-	return imported_file_dst_path
+	return imported_file_path
