40e0548e6370dc59aff8aba7363a1de1a3d92d19
Kai Roth <kai@kana.jetzt>
2023-03-07 17:22:34 +0100

    refactor: :fire: Removed unused `is_manifest_valid()` function

--- addons/mod_tool/manifest_editor.gd
+++ addons/mod_tool/manifest_editor.gd
@@ -36,23 +36,6 @@ func save_manifest() -> void:
 	# TODO: Save manifest dict to manifest.json in mod directory
 
 
-func is_manifest_valid() -> bool:
-	var mod_manifest: Script
-	if ModLoaderUtils.file_exists("res://addons/mod_loader/mod_manifest.gd"):
-		mod_manifest = load("res://addons/mod_loader/mod_manifest.gd")
-
-	var is_valid: bool
-	if not mod_manifest:
-		return false
-
-	var mod_name: String = ModToolStore.name_mod_dir
-	is_valid = input_name.show_error_if_not(mod_manifest.is_name_or_namespace_valid(mod_name))
-
-	#todo
-
-	return is_valid
-
-
 func update_ui() -> void:
 	for input in input_fields:
 		if manifest_data_dict.has(input.key):
