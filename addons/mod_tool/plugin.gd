035b741fa530161af5a9db12ac6ac9545c97973b
Qubus0 <steen.rickmer@gmx.de>
2023-03-15 12:36:58 +0100

    feat: change dock path

--- addons/mod_tool/plugin.gd
+++ addons/mod_tool/plugin.gd
@@ -7,7 +7,7 @@ var dock: Control
 func _enter_tree() -> void:
 	add_autoload_singleton('ModToolStore', "res://addons/mod_tool/store.gd")
 
-	dock = preload("res://addons/mod_tool/dock.tscn").instance()
+	dock = preload("res://addons/mod_tool/interface/dock/dock.gd").instance()
 	dock.editor_plugin = self
 	add_control_to_bottom_panel(dock, 'Mod Tool')
 
