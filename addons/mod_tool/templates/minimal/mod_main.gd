67e79c3848470d9d4b4ceddd9ff934c5dd6f44e1
Qubus0 <steen.rickmer@gmx.de>
2024-12-02 15:57:14 +0100

    chore: update templates

--- addons/mod_tool/templates/minimal/mod_main.gd
+++ addons/mod_tool/templates/minimal/mod_main.gd
@@ -1,8 +1,8 @@
 extends Node
 
 
-const AUTHORNAME_MODNAME_DIR := "AuthorName-ModName"
-const AUTHORNAME_MODNAME_LOG_NAME := "AuthorName-ModName:Main"
+const MOD_DIR := "AuthorName-ModName"
+const LOG_NAME := "AuthorName-ModName:Main"
 
 var mod_dir_path := ""
 var extensions_dir_path := ""
@@ -13,6 +13,8 @@ func _init() -> void:
 	mod_dir_path = ModLoaderMod.get_unpacked_dir().path_join(AUTHORNAME_MODNAME_DIR)
 	# Add extensions
 	install_script_extensions()
+	install_script_hook_files()
+
 	# Add translations
 	add_translations()
 
@@ -20,6 +22,8 @@ func _init() -> void:
 func install_script_extensions() -> void:
 	extensions_dir_path = mod_dir_path.path_join("extensions")
 
+func install_script_hook_files() -> void:
+	extensions_dir_path = mod_dir_path.path_join("extensions")
 
 func add_translations() -> void:
 	translations_dir_path = mod_dir_path.path_join("translations")
